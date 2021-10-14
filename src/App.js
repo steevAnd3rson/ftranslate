1236d32b17ce6bf669c42fd4409aefdf8ed3fb81
steevAnd3rson <steev.and3rson@gmail.com>
2021-10-14 19:04:18 +0200

    version 1.5

--- src/App.js
+++ src/App.js
@@ -1,28 +1,52 @@
 
+import React, {useState} from 'react';
 import Components from './components/Components';
-
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 class="title">F·Translate</h1>
 
+  const [cont, setCont] = useState('')
+
+  const lol = document.getElementById("texto");
+  const rango = document.getElementById("rango");
+  
+  let acho = cont.replaceAll('a', 'i').replaceAll('e','i').replaceAll('o', 'i').replaceAll('u','i')
+ 
+  function texto(){
+    playText(lol.value)
+  }
+  function playText(text){
+    const utterance = new SpeechSynthesisUtterance(text)
+    utterance.rate = rango.value || 1
+    speechSynthesis.speak(utterance)
+  }
+
+
+  return (
+
+    <div className="App">
+
+      <header className="App-header">
+        <h1 className="title"
+        onClick={texto}>F·Translate</h1>
+
+        <div>
+
+        <input type="range" id="rango" min=".5" max="2.5"></input>
+        
+        </div>
 
       </header>
 
       <section>
-
-        <form>
-          <textarea></textarea>
-        </form>
-
-        <Components /> 
+            
+              <textarea onChange={ (e) => setCont(e.target.value) } ></textarea>
+                
+              <textarea  value={acho} id="texto"></textarea>
+              
 
       </section> 
       
-
     </div>
   );
 }
--- src/components/Components.jsx
+++ src/components/Components.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import App from '../App';
+
 function Components(){
+
+
     return(
-        <div>
-            body
-        </div>
+        <div></div>
     )
 }
+
+
 
 export default Components;
